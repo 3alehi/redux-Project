a9e27ba2e3c360c6265a2e05c3dc0321c0e8e200
Slavik <slavikmasih@gmail.com>
2023-11-04 12:03:54 +0330

    update

--- TodoList/script.js
+++ TodoList/script.js
@@ -2,7 +2,7 @@ import { addTodo } from "./Redux/actionsType.js"
 import { setaddTodo } from "./Redux/action.js"
 let btn = document.getElementById("add-btn")
 let input = document.getElementById("input")
-
+let Show_todo = document.getElementById("Show-todo")
 const TodoList = (state = [] , action)=>{
   switch(action.type){
     case addTodo :{
@@ -30,8 +30,27 @@ btn.addEventListener("click" , e =>{
   let inputValue = input.value
   CreateStoreTotoList.dispatch(setaddTodo(inputValue));
   input.value = ""
+  Show_todo.innerHTML =""
 
   CreateStoreTotoList.getState().forEach(element => {
+    Show_todo.insertAdjacentHTML("afterbegin",`
+    
+    <div class="todo">
+    <h4>    ${element.title}
+    <h4/>
+    <div>
+    ${element.isComplated == false ? "تکمیل نشده" : "تکمیل شده"}
+    <div/>
+    
+    
+    
+    </div>
+    
+    
+    
+    
+    
+    `)
     console.log(element)
   });
 
